f36d0104d6fee895cb3adbdc8e413daccf9211e6
poovarasan <poovarasan.official@gmail.com>
2023-02-20 06:40:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let start=document.querySelector("h1");
+setTimeout(()=>{
+    start.innerText="9";
+    setTimeout(()=>{
+        start.innerText="8";
+        setTimeout(()=>{
+            start.innerText="7";
+            setTimeout(()=>{
+                start.innerText="6";
+                setTimeout(()=>{
+                    start.innerText="5";
+                    setTimeout(()=>{
+                        start.innerText="4";
+                        setTimeout(()=>{
+                            start.innerText="3";
+                            setTimeout(()=>{
+                                start.innerText="2";
+                                setTimeout(()=>{
+                                    start.innerText="1";
+                                    setTimeout(()=>{
+                                        start.innerText="Happy Independence Day🎉🎊";
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
